1b571c1d3d288fc14b493efc0ce6ff6f88ec8742
venkatesh <136556477+venkatesh209@users.noreply.github.com>
2024-01-27 14:32:18 +0530

    form

--- src/redux/action.js
+++
@@ -1,9 +0,0 @@
-export const updateText = (textData) => ({
-    type: 'UPDATE_TEXT',
-    payload: textData,
-  });
-  
-  export const updateStyle = (styleData) => ({
-    type: 'UPDATE_STYLE',
-    payload: styleData,
-  });
